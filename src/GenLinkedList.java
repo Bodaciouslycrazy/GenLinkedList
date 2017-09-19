db2000c848901012ed50cb8af997635e6fe38319
Bodaciouslycrazy <bodiemalik@gmail.com>
2017-09-18 21:39:57 -0500

    finished

--- src/GenLinkedList.java
+++ src/GenLinkedList.java
@@ -1,22 +1,31 @@
 import java.util.*;
 import java.lang.StringBuilder;
 
-/* Bodie Malik
+/* PROJECT 1
+ * GENLINKEDLIST
+ * 
+ * Bodie Malik
  * Sun September 17
  * 
  * CS 3345.002
  * 
+ * Notes:
+ * Some of these function return the linked list. This is to allow for chaining methods. ex:
+ * mylist.addFront(5).addFront(6).addEnd(4);
+ * etc...
  * 
  */
 
-public class GenLinkedList<T> {
+public class GenLinkedList<T>
+{
 	
 	private int length = 0;
 	Node front;
 	Node back;
+	private final boolean extraPrint = true;
 	
 	//runs in constant time
-	public void addFront(T item)
+	public GenLinkedList<T> addFront(T item) //Done
 	{
 		if(length == 0)
 		{
@@ -30,10 +39,12 @@ public class GenLinkedList<T> {
 		}
 		
 		length++;
+		
+		return this;
 	}
 	
 	//runs in constant time
-	public void addEnd(T item)
+	public GenLinkedList<T> addEnd(T item) //Done
 	{
 		if(length == 0)
 		{
@@ -48,10 +59,12 @@ public class GenLinkedList<T> {
 		}
 		
 		length++;
+		
+		return this;
 	}
 	
 	//runs in constant time
-	public void removeFront() throws Exception
+	public GenLinkedList<T> removeFront() throws Exception //Done
 	{
 		if(length <= 0)
 		{
@@ -67,10 +80,12 @@ public class GenLinkedList<T> {
 			front = front.getNext();
 			length--;
 		}
+		
+		return this;
 	}
 	
 	//runs in O(N)
-	public void removeEnd() throws Exception
+	public GenLinkedList<T> removeEnd() throws Exception //Done
 	{
 		if(length == 0)
 		{
@@ -96,9 +111,11 @@ public class GenLinkedList<T> {
 			n.setNext(null);
 			length--;
 		}
+		
+		return this;
 	}
 	
-	public void set(int pos, T item) throws Exception
+	public void set(int pos, T item) throws Exception //Done
 	{
 		if(pos >= length)
 		{
@@ -117,7 +134,7 @@ public class GenLinkedList<T> {
 		}
 	}
 	
-	public T get(int pos) throws Exception
+	public T get(int pos) throws Exception //Done
 	{
 		if(pos >= length )
 		{
@@ -137,7 +154,7 @@ public class GenLinkedList<T> {
 	
 	
 	//Swap does not move any of the nodes, but only moves the values stored in them.
-	public void swap(int pos1, int pos2) throws Exception
+	public void swap(int pos1, int pos2) throws Exception //Done
 	{
 		if(pos1 >= length || pos2 >= length)
 		{
@@ -177,11 +194,11 @@ public class GenLinkedList<T> {
 		
 	}
 	
-	public void shift(int amt) throws Exception//cyclic!
+	public void shift(int amt) throws Exception //Done
 	{
 		if(Math.abs(amt) >= length)
 		{
-			throw new Exception("Can't shift larger than the list!");
+			throw new Exception("Can't shift larger than the list! (Modulus maybe?)");
 		}
 		
 		back.setNext(front); //this makes the linked list cycle
@@ -206,7 +223,7 @@ public class GenLinkedList<T> {
 		
 	}
 	
-	public void removeMatching(T item)
+	public void removeMatching(T item) //Done
 	{
 		if(length == 0)
 			return;
@@ -243,11 +260,11 @@ public class GenLinkedList<T> {
 		}
 	}
 	
-	public void erase(int pos, int num) throws Exception
+	public void erase(int pos, int num) throws Exception //Done
 	{
-		if(num <= 0)
+		if(num <= 0 || pos < 0)
 		{
-			throw new Exception("Invalid number size.");
+			throw new Exception("Invalid input.");
 		}
 		else if( pos + num - 1 >= length)
 		{
@@ -294,50 +311,81 @@ public class GenLinkedList<T> {
 		}
 	}
 	
-	public void insertList( List<T> list, int pos) throws Exception
+	public void insertList( List<T> list, int pos) throws Exception //Done
 	{
 		if(pos > length)
 		{
 			throw new Exception("Index out of bounds.");
 		}
+		else if(list.size() <= 0)
+		{
+			return;
+		}
 
-		Node begin = front;
-		for(int i = 0; i < pos; i++)
-		{
-			begin = begin.getNext();
-		}
-		Node after = begin.getNext();
 		
-		for(int i = 0; i < list.size(); i++)
+		Node inserta = new Node(list.get(0));
+		Node insertb = inserta;
+		int listPos = 1;
+		while(listPos < list.size())
 		{
-			Node temp = new Node(list.get(i));
-			begin.setNext(temp);
-			begin = begin.getNext();
+			insertb.setNext( new Node(list.get(listPos)));
+			insertb = insertb.getNext();
+			listPos++;
 		}
 		
-		if(after != null )
+		//inserta is pointing at the first of the new nodes, insertb is pointing at the last of the new nodes
+		
+		//lots of special cases
+		if(length == 0)
 		{
-			begin.setNext(after);
+			front = inserta;
+			back = insertb;
+		}
+		else if(pos == 0)
+		{
+			insertb.setNext(front);
+			front = inserta;
+		}
+		else if(pos == length)
+		{
+			back.setNext(inserta);
+			back = insertb;
 		}
 		else
 		{
-			back = begin;
+			//find the node at pos and pos+1
+			Node n0 = front;
+			for(int i = 0; i < pos; i++)
+			{
+				n0 = n0.getNext();
+			}
+			Node n1 = n0.getNext();
+			
+			n0.setNext(inserta);
+			insertb.setNext(n1);
 		}
 		
 		length += list.size();
 	}
 	
-	//Methods not part of assignment requirements
+	
+	
+	
+	//************Other Methods***************
+	
+	
+	
 	public String toString()
 	{
+		StringBuilder sb = new StringBuilder();
+		
 		
 		if(length == 0)
 		{
-			return "[ ]";
+			sb.append("[ ]");
 		}
 		else
 		{
-			StringBuilder sb = new StringBuilder();
 			sb.append("[ ");
 			
 			Node n = front;
@@ -350,8 +398,22 @@ public class GenLinkedList<T> {
 			}
 			
 			sb.append("]");
-			return sb.toString();
 		}
+		
+		if(extraPrint) //prints extra information about where the front and end are pointing to, and the length.
+		{
+			String f = "null";
+			String b = "null";
+			
+			if(front != null)
+				f = front.get().toString();
+			if(back != null)
+				b = back.get().toString();
+			
+			sb.append(" F: ").append(f).append(" B: ").append(b).append(" L: ").append(length);
+		}
+		
+		return sb.toString();
 	}
 	
 	public void clear()
@@ -363,59 +425,205 @@ public class GenLinkedList<T> {
 	
 	public static void main(String[] args) throws Exception
 	{
+		System.out.println("Welcome to Bodie's GenLinkedList!");
+		System.out.println("There are some things to note about this implementation:");
+		System.out.println("1 - some of the basic adding/removing commands return a reference of the list. This is to allow for chaining methods.");
+		System.out.println("2 - the toString() method will add some extra things to the string. In addition to printing the list, it also prints the front (F) back (B) and length (L) of the list to help understand what is happening.");
+		System.out.println("\tYou can change this by turning the value \"extraPrint\" to false.\n");
 		System.out.println("*****Testing GenLinkedList*****");
-		GenLinkedList<String> sl = new GenLinkedList<String>();
-		System.out.println("Testing addEnd()");
-		sl.addEnd("one");
-		sl.addEnd("two");
-		sl.addEnd("three");
-		sl.addEnd("four");
-		sl.addEnd("five");
-		System.out.println(sl.toString());
-		System.out.println("Testing addFront()");
-		sl.addFront("bob");
-		sl.addFront("joe");
-		sl.addFront("bill");
-		System.out.println(sl.toString());
-		System.out.println("Testing removeFront and removeEnd");
-		sl.removeFront();
-		sl.removeEnd();
-		System.out.println(sl.toString());
-		System.out.println("Testing set");
-		sl.set(5, "FOUR");
-		System.out.println(sl.toString());
-		System.out.println("testing get");
-		System.out.println(sl.get(2));
-		System.out.println("Testing swap");
-		sl.swap(0, 1);
-		sl.swap(3, 2);
-		sl.swap(4, 4);
-		System.out.println(sl.toString());
-		
-		System.out.println("*****INTEGER LINKED LIST*****");
 		GenLinkedList<Integer> il = new GenLinkedList<Integer>();
-		il.addEnd(0);
-		il.addEnd(1);
-		il.addEnd(2);
+		
+		//Add Front
+		System.out.println("Testing addFront");
+		for(int i = 0; i < 10; i++)
+		{
+			il.addFront((int)(Math.random() * 10));
+		}
+		System.out.println(il);
+		
+		//Add end
+		System.out.println("Testing addEnd");
+		for(int i = 0; i < 10; i++)
+		{
+			il.addEnd((int)(Math.random() * 10 + 10));
+		}
+		System.out.println(il);
+		
+		//remove front
+		System.out.println("Testing removeFront");
+		for(int i = 0; i < 20; i++)
+		{
+			il.removeFront();
+		}
+		System.out.println(il);
+		
+		//remove back
+		System.out.println("Adding some elements for more testing");
 		il.addEnd(3);
 		il.addEnd(4);
-		System.out.println(il.toString());
-		System.out.println("Testing erase");
-		il.erase(1, 2);
-		System.out.println(il);
 		il.addEnd(5);
-		il.addEnd(6);
 		System.out.println(il);
-		il.erase(2, 3);
-		System.out.println(il);
-		il.addEnd(7);
-		System.out.println(il);
-		il.erase(0, 1);
-		System.out.println(il);
-		il.erase(0, 2);
+		System.out.println("Testing removeEnd");
+		il.removeEnd();
+		il.removeEnd();
+		il.removeEnd();
 		System.out.println(il);
 		
-		System.out.println("Done testing");
+		//error testing
+		System.out.println("Error Testing");
+		try
+		{
+			il.removeFront();
+		}
+		catch(Exception e)
+		{
+			System.out.println("Correct Exception caught: " + e.toString());
+		}
+		try
+		{
+			il.removeEnd();
+		}
+		catch(Exception e)
+		{
+			System.out.println("Correct Exception caught: " + e.toString());
+		}
+		
+		//test insert list
+		System.out.println("Testing insertList");
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		for(int i = 0; i < 10; i++)
+		{
+			al.add(i);
+		}
+		
+		il.insertList(al, 0);
+		System.out.println(il);
+		
+		al = new ArrayList<Integer>();
+		for(int i = 0; i < 3; i++)
+		{
+			al.add(i+10);
+		}
+		
+		il.insertList(al, 0);
+		System.out.println(il);
+		il.insertList(al, 5);
+		System.out.println(il);
+		il.insertList(al, 16);
+		System.out.println(il);
+		
+		//test get and set
+		System.out.println("Testing get and set");
+		System.out.println("The third element is: " + il.get(2));
+		System.out.println("Setting third element to 30.");
+		il.set(2, 30);
+		System.out.println(il);
+		
+		//test swap
+		System.out.println("Testing swap");
+		System.out.println(il);
+		il.swap(0, 1);
+		System.out.println(il);
+		il.swap(3,2);
+		System.out.println(il);
+		il.swap(4,4);
+		System.out.println(il);
+		
+		il.clear();
+		System.out.println("Testing shift");
+		for(int i = 0; i < 10; i++)
+		{
+			il.addEnd(i);
+		}
+		System.out.println(il);
+		il.shift(3);
+		System.out.println(il);
+		il.shift(-2);
+		System.out.println(il);
+		
+		try
+		{
+			il.shift(11);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Correct Exception caught: " + e.toString());
+		}
+		
+		System.out.println("Testing removeMatching");
+		il.addEnd(7).addEnd(1).addEnd(11).addEnd(4);
+		il.addFront(11);
+		System.out.println(il);
+		il.removeMatching(7);
+		System.out.println(il);
+		il.removeMatching(11);
+		System.out.println(il);
+		il.removeMatching(4);
+		System.out.println(il);
+		il.removeMatching(1);
+		System.out.println(il);
+		il.removeMatching(50);
+		System.out.println(il);
+		
+		System.out.println("Testing erase");
+		il.clear();
+		for(int i = 0; i < 20; i++)
+		{
+			il.addEnd(i);
+		}
+		System.out.println(il);
+		
+		il.erase(0, 3);
+		System.out.println(il);
+		il.erase(14, 3);
+		System.out.println(il);
+		il.erase(5, 3);
+		System.out.println(il);
+		il.erase(0, 10);
+		System.out.println(il);
+		
+		
+		try
+		{
+			il.erase(5, 1);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Correct Exception caught: " + e.toString());
+		}
+		try
+		{
+			il.erase(0, 2);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Correct Exception caught: " + e.toString());
+		}
+		try
+		{
+			il.erase(-1, 1);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Correct Exception caught: " + e.toString());
+		}
+		try 
+		{
+			il.erase(0, -1);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Correct Exception caught: " + e.toString());
+		}
+		
+		il.erase(0, 1);
+		System.out.println(il);
+		
+		System.out.println("*****DONE TESTING*****");
+		System.out.println("Feel free to add your own code to test this linked list.");
+		
+		
+		
 	}
 	
 
